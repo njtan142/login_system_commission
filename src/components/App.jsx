efce7fc5626290f72882d05cdbbdc5b699450e7c
Niño James A. Tan <njtan142@gmail.com>
2022-08-30 11:54:28 +0800

    firebase initialized

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Form, Card} from "react-bootstrap";
+
+
+
+const App = (props) => {
+    return(
+        <>
+        <Card></Card>
+        <div>Already have an account? <a href="/login">Login</a></div>
+        </>
+    )
+}
+
+
+
+export default App;
